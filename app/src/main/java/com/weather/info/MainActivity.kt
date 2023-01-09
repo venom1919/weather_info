8c1f7818b8db913cda3d665fceeb7a4b849dff8b
Venom <lyaskivskii@gmail.com>
2023-01-09 17:40:41 +0200

    start pr

---
+++ app/src/main/java/com/weather/info/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.weather.info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
